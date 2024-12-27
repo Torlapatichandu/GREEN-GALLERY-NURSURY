ade0ef82354a3f4e344d143219e2794dc7133105
Torlapatichandu <tchandu9849@gmail.com>
2024-12-27 20:39:28 +0530

    Initial commit

---
+++ FSD/WEB-INF/src/al.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class al {
+    public static void main(String[] args) {
+        // Display an alert dialog with a message
+        JOptionPane.showMessageDialog(null, "This is an alert message", "Alert", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
